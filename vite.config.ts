6f618d0c4d2b6f7be89a97376f308fc1a2320dd2
deepaktyagi1103 <143095686+deepaktyagi1103@users.noreply.github.com>
2025-11-27 08:48:44 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/getcrrllp/",   // ← GitHub Pages deployment ke liye required
+  base: '/getcrrllp/',  // *** ONLY THIS REQUIRED ***
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
